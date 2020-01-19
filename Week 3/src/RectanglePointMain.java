27d50ae24b2acacd02e4264cc6a1f56c246285a1
jinfayap <jinfayap@gmail.com>
2020-01-19 20:43:41 +0800

    Add RectanglePointMain.java

---
+++ Week 3/src/RectanglePointMain.java
@@ -0,0 +1,27 @@
+import java.awt.Point;
+import java.awt.Rectangle;
+
+public class RectanglePointMain {
+    public static void main (String[] args){
+        Point p1 = new Point(0, 0);
+        Rectangle r1 = new Rectangle(2, 3, 5, 6);
+        System.out.println("arguments: " + p1 + ", " + r1);
+
+        Point p2 = move(p1, r1);
+        System.out.println("argument point after method call: " + p1);
+        System.out.println("argument rectangle after method call: " + r1);
+        System.out.println("returned point: " + p2);
+
+        System.out.println(move(null, null));
+    }
+    public static Point move(Point p, Rectangle r){
+        if (r == null || p == null){
+            return null;
+        }
+        Point obj = new Point(r.x, r.y);
+        r.x = p.x;
+        r.y = p.y;
+
+        return obj;
+    }
+}
