3de8041eadcaeb224433d760025d79608cea5c7d
jinfayap <jinfayap@gmail.com>
2020-01-19 20:44:39 +0800

    PrintPriceMain.java

---
+++ Week 3/src/PrintPriceMain.java
@@ -0,0 +1,17 @@
+// [Key Exercise] printPrice
+public class PrintPriceMain {
+    public static void printPrice(String item) {
+        // TODO: add your code here
+        int dividerPosition = item.indexOf("--");
+        String itemName = item.substring(0, dividerPosition);
+        String itemPrice = item.substring(dividerPosition + 3);
+        itemPrice = itemPrice.replace("/", ".");
+        System.out.println(itemName.toUpperCase().trim() + ": " +  itemPrice);
+    }
+
+    public static void main(String[] args) {
+        printPrice("sandwich  --$4/50");
+        printPrice("  soda --$10/00");
+        printPrice("  fries --$0/50");
+    }
+}
