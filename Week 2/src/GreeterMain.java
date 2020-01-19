a4c2dd74a904da02f861b79902defe6b1cb2a666
jinfayap <jinfayap@gmail.com>
2020-01-19 23:59:24 +0800

    Add GreaterMain.java

--- Week 2/src/GreeterMain.java
+++ Week 2/src/GreeterMain.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class GreeterMain {
+    public static void main (String[] args){
+        String first = args[0];
+        String second = args[1];
+
+        if (args.length == 2){
+            if (second.equals("M")){
+                System.out.println("Dear Mr. " + first);
+            } else {
+                System.out.println("Dear Mdm. " + first);
+            }
+        }else {
+            System.out.println("Dear " + first + " family");
+        }
+    }
 }
