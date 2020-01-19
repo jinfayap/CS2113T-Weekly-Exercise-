33844046fc645d219c4f3378ddfd661017082305
jinfayap <jinfayap@gmail.com>
2020-01-19 20:44:50 +0800

    Add FilteremailsMain.java

---
+++ Week 3/src/FilterEmailsMain.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class FilterEmailsMain {
+    public static String[] filterEmails(String[] items){
+        // TODO: add your code here
+        String[] filteredEmails = new String[items.length];
+        int matchedCount = 0;
+        for (String item : items){
+            if(item.contains("@")){
+                filteredEmails[matchedCount] = item;
+                matchedCount++;
+            }
+        }
+        return Arrays.copyOf(filteredEmails, matchedCount);
+    }
+
+    public static void printItems(String[] items){
+        // TODO: add your code here
+        System.out.println(Arrays.toString(items));
+    }
+
+    public static void main(String[] args) {
+        printItems(filterEmails(new String[]{}));
+        printItems(filterEmails(new String[]{"abc"}));
+        printItems(filterEmails(new String[]{"adam@example.com", "aab", "john@example.com", "some@"}));
+        printItems(filterEmails(new String[]{"xyz", "@bee.com", "aab"}));
+    }
+}
