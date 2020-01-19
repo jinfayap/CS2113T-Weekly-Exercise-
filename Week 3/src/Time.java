ce3c37495fcb0436e6e0793a23eff7eec23f4ce6
jinfayap <jinfayap@gmail.com>
2020-01-19 20:43:30 +0800

    Add Time.java Class

---
+++ Week 3/src/Time.java
@@ -0,0 +1,33 @@
+public class Time {
+    private int hour;
+    private int minute;
+    private int second;
+
+    public Time(int hour, int minute, int second){
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public void getTime(){
+        System.out.println("The time now is " + this.hour + ":" + this.minute + ":" + this.second);
+    }
+    public int getHour(){
+        return hour;
+    }
+    public int getMinute(){
+        return minute;
+    }
+    public int getSecond(){
+        return second;
+    }
+    public void setHour(int hour){
+        this.hour = hour;
+    }
+    public void setMinute(int minute){
+        this.minute = minute;
+    }
+    public void setSecond(){
+        this.second = second;
+    }
+}
