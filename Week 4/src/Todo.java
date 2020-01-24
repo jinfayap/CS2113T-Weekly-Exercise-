44b18482009ee85289c642eda8b8680356fd92ea
jinfayap <jinfayap@gmail.com>
2020-01-25 02:02:02 +0800

    Add Todo.java

---
+++ Week 4/src/Todo.java
@@ -0,0 +1,16 @@
+public class Todo extends Task {
+    protected boolean toDoDone;
+
+    public Todo(String description){
+        super(description);
+        this.toDoDone = false;
+    }
+    public boolean isDone(){
+        return toDoDone;
+    }
+    public void setDone(boolean toDoDone){
+        this.toDoDone = true;
+    }
+
+}
+
