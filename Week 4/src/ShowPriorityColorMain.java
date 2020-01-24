131b6f68374078f3a6d90862d493a716089ac8e1
jinfayap <jinfayap@gmail.com>
2020-01-25 02:01:16 +0800

    Add ShowPriorityColorMain.java

---
+++ Week 4/src/ShowPriorityColorMain.java
@@ -0,0 +1,14 @@
+public class ShowPriorityColorMain {
+
+    public static void describe(String color, Priority level) {
+
+        System.out.println(color + " indicates " + level  + " priority");
+    }
+
+    public static void main(String[] args) {
+        describe("Red", Priority.HIGH);
+        describe("Orange", Priority.MEDIUM);
+        describe("Blue", Priority.MEDIUM);
+        describe("Green", Priority.LOW);
+    }
+}
