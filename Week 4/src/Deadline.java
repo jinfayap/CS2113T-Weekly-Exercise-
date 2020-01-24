ab4374cba903b2d6ee360a788669e07a51c381db
jinfayap <jinfayap@gmail.com>
2020-01-25 02:00:28 +0800

    Add Deadline.java

---
+++ Week 4/src/Deadline.java
@@ -0,0 +1,15 @@
+public class Deadline extends Todo {
+    private String by;
+
+    public Deadline(String description, String by){
+        super(description);
+        this.by = by;
+        this.toDoDone = false;
+    }
+    public String getBy(){
+        return by;
+    }
+    public void setBy(String by){
+        this.by = by;
+    }
+}
