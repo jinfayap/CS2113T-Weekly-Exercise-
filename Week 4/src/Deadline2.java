9035486cbdceb05177379bab10bc078242cc6859
jinfayap <jinfayap@gmail.com>
2020-01-25 01:58:50 +0800

    Add Deadline2.java

---
+++ Week 4/src/Deadline2.java
@@ -0,0 +1,21 @@
+public class Deadline2 extends Todo2 {
+
+    protected String by;
+
+    public Deadline2(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    public void setBy(String by) {
+        this.by = by;
+    }
+
+    public String getBy() {
+        return by;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + System.lineSeparator() + "do by: " + by;
+    }
+}
