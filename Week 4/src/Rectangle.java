c2597fa401c978646ae022297e0caff8f297924f
jinfayap <jinfayap@gmail.com>
2020-01-25 01:57:15 +0800

    Add Rectangle.java

---
+++ Week 4/src/Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle extends Shape{
+    private int height;
+    private int width;
+
+    public Rectangle(int height, int width){
+        this.height = height;
+        this.width = width;
+    }
+
+    public int area(){
+        return height * width;
+    }
+}
